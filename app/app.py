79cc85b259b9bc6a88e7b52cf4a41d61f7ed4ebc
Ruslan <sounditbox@gmail.com>
2025-02-14 20:14:53 +0400

    added logs

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 import uuid
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from loguru import logger
 
 SERVER_ADDRESS = ('localhost', 8000)
 ALLOWED_EXTENSIONS = ('jpg', 'jpeg', 'png', 'gif')
 ALLOWED_LENGTH = (5 * 1024 * 1024)
+
+logger.add('logs/app.log', format="[{time: YYYY-MM-DD HH:mm:ss}] | {level} | {message}")
 
 
 class ImageHostingHandler(BaseHTTPRequestHandler):
@@ -11,27 +14,33 @@ class ImageHostingHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path in ('/', '/index.html'):
+            logger.info(f'GET {self.path}')
             self.send_response(200)
             self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
             self.wfile.write(open('index.html', 'rb').read())
         else:
+            logger.warning(f'GET {self.path}')
             self.send_response(404, 'Not Found')
 
     def do_POST(self):
         if self.path == '/upload':
+            logger.info(f'POST {self.path}')
             content_length = int(self.headers.get('Content-Length'))
             if content_length > ALLOWED_LENGTH:
+                logger.error('Payload Too Large')
                 self.send_response(413, 'Payload Too Large')
                 return
 
             filename = self.headers.get('Filename')
 
             if not filename:
+                logger.error('Lack of Filename header')
                 self.send_response(400, 'Lack of Filename header')
                 return
-            filename, ext = filename.split('\\')[-1].split('.')
+            filename, ext = filename.split('.')
             if ext not in ALLOWED_EXTENSIONS:
+                logger.error('Unsupported file extension')
                 self.send_response(400, 'Unsupported file extension')
                 return
 
@@ -41,6 +50,7 @@ class ImageHostingHandler(BaseHTTPRequestHandler):
             with open(f'images/{image_id}.{ext}', 'wb') as f:
                 f.write(data)
 
+            logger.info(f'Upload success: {image_id}.{ext}')
             self.send_response(201)
             self.send_header('Location',
                              f'http://{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}/images/{filename}.{ext}')
@@ -55,12 +65,12 @@ class ImageHostingHandler(BaseHTTPRequestHandler):
 def run():
     httpd = HTTPServer(SERVER_ADDRESS, ImageHostingHandler)
     try:
-        print(f'Serving at http://{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}')
+        logger.info(f'Serving at http://{SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}')
         httpd.serve_forever()
     except Exception:
         pass
     finally:
-        print('Server stopped')
+        logger.info('Server stopped')
         httpd.server_close()
 
 
