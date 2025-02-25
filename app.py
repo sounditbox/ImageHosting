44bb408245bf167239603b1172d8af077b3be805
Ruslan <sounditbox@gmail.com>
2025-02-25 21:18:45 +0400

    fixed project structure

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ class ImageHostingHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json; charset=utf-8')
         self.end_headers()
 
-        images = [f for f in listdir('./images') if isfile(join('./images', f))]
+        images = [f for f in listdir('images') if isfile(join('images', f))]
         self.wfile.write(json.dumps({'images': images}).encode('utf-8'))
 
     def get_upload(self):
