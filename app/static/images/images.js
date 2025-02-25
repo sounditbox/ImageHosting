c19f0281057700906b221327823958e1d3f600bd
Ruslan <sounditbox@gmail.com>
2025-02-25 15:22:54 +0400

    Made images clickable

--- app/static/images/images.js
+++ app/static/images/images.js
@@ -3,9 +3,14 @@ fetch('http://localhost:8000/images')
     .then(data => {
         const imagesContainer = document.getElementById('images');
         data.images.forEach(image => {
+            const a = document.createElement('a');
+            a.href = `/images/${image}`;
+            imagesContainer.appendChild(a);
+
             const imageElement = document.createElement('img');
             imageElement.src = `/images/${image}`;
             imageElement.alt = image;
-            imagesContainer.appendChild(imageElement);
+
+            a.appendChild(imageElement);
         });
     })
